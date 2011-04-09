2404145cb857b6c32d4b4d65fb679b4e2f14c837
Eric <Eric@Eric-Dell.hsd1.or.comcast.net.>
2011-04-09 08:45:32 -0700

    Add framework loader.

--- includes/class-admin.php
+++ includes/class-admin.php
@@ -1,7 +1,28 @@
 <?php
-if ( !class_exists('WPCrossfire_Admin') ) :
+/**
+ * Backend class for creating administrative interfaces and handling options.
+ * Version 0.1
+ */
 
-class WPCrossfire_Admin {
+/**
+ * Copyright 2011  Eric Mann  (email : eric@eamann.com)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+if ( !class_exists('JDM_Admin') ) :
+
+class JDM_Admin {
 
 }
 
--- wp-crossfire.php
+++ wp-crossfire.php
@@ -9,21 +9,22 @@ Author URI: http://www.eamann.com
 License: GPLv3+
 */
 
-/*  Copyright 2011  Eric Mann  (email : eric@eamann.com)
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/**
+ * Copyright 2011  Eric Mann  (email : eric@eamann.com)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 load_plugin_textdomain( 'wp-crossfire', false, dirname( plugin_basename( __FILE__ ) ) . '/i18n/' );
 
 
@@ -39,4 +40,6 @@ define( 'WPCROSSFIRE_BASENAME', plugin_basename(__FILE__) );
 
 define( 'WPCROSSFIRE_VERSION', '0.1' );
 
-require_once('includes/class-admin.php');
+// Register plugin frameworks an dependencies
+require_once('includes/class-wpframeworks.php');
+wp_register_framework('jdm-admin', WPCROSSFIRE_PATH . '/includes/class-admin.php', '', '0.1');
